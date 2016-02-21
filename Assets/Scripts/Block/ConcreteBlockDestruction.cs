4795a39a4733215e67b34e84dc19f63458f72bab
oozzy <oozzy.tlo@gmail.com>
2016-02-20 21:21:18 -0800

    Add timer to block destruction

--- Assets/Scripts/Block/ConcreteBlockDestruction.cs
+++ Assets/Scripts/Block/ConcreteBlockDestruction.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class ConcreteBlockDestruction : MonoBehaviour {
 	public Rigidbody player;
+	bool timerStarted = false;
+	float timeLeft = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -12,7 +14,13 @@ public class ConcreteBlockDestruction : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (player.transform.position.z > transform.position.z) {
-			Destroy(gameObject);
+			timerStarted = true;
+		}
+		if (timerStarted) {
+			timeLeft -= Time.deltaTime;
+			if (timeLeft < 0) {
+				Destroy (gameObject);
+			}
 		}
 	}
 }
--- Assets/Scripts/Block/IceBlockDestruction.cs
+++ Assets/Scripts/Block/IceBlockDestruction.cs
@@ -3,16 +3,24 @@ using System.Collections;
 
 public class IceBlockDestruction : MonoBehaviour {
 	public Rigidbody player;
+	bool timerStarted = false;
+	float timeLeft = 5;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (player.transform.position.z > transform.position.z) {
-			Destroy(gameObject);
+			timerStarted = true;
+		}
+		if (timerStarted) {
+			timeLeft -= Time.deltaTime;
+			if (timeLeft < 0) {
+				Destroy (gameObject);
+			}
 		}
 	}
 }
