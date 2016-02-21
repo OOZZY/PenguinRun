b663820aea2d4ae222ad9f4b5646067fe4acdbdb
oozzy <oozzy.tlo@gmail.com>
2016-02-21 14:37:30 -0800

    Add generation of ice blocks with items

--- Assets/Scripts/Block/BlockGeneration.cs
+++ Assets/Scripts/Block/BlockGeneration.cs
@@ -8,14 +8,41 @@ public class BlockGeneration : MonoBehaviour {
 	public Rigidbody player;
 	public float spawnHeight = 15;
 	public float spawnDistance = 30;
+	public Transform fish;
+	public Transform fishBone;
+	public Transform heart;
+	public Transform skull;
+	public Transform star;
 
 	void SpawnBlock(Vector3 position) {
-		if (Random.Range (0, 100) < 49) {
+		if (Random.Range (0, 100) < 50) {
 			Rigidbody concreteClone = (Rigidbody)Instantiate (concreteBlock, position, new Quaternion ());
 			concreteClone.GetComponent<ConcreteBlockDestruction> ().player = player;
 		} else {
-			Rigidbody iceClone = (Rigidbody)Instantiate (iceBlock, position, new Quaternion ());
+			Rigidbody iceClone = (Rigidbody)Instantiate (iceBlock, position, new Quaternion (0, 0, 0, 1));
 			iceClone.GetComponent<IceBlockDestruction> ().player = player;
+
+			float num = Random.Range (0, 100);
+			Transform toBeCloned = null;
+			Quaternion newQuaternion = new Quaternion (0, 0, 0, 1);
+			if (num < 50) {
+			} else if (num < 60) {
+				toBeCloned = fish;
+			} else if (num < 70) {
+				toBeCloned = fishBone;
+			} else if (num < 80) {
+				toBeCloned = heart;
+			} else if (num < 90) {
+				toBeCloned = skull;
+				newQuaternion.y = -180;
+			} else if (num < 100) {
+				toBeCloned = star;
+			}
+
+			if (toBeCloned != null) {
+				Transform child = (Transform)Instantiate (toBeCloned, position, newQuaternion);
+				child.parent = iceClone.GetComponent<Transform> ();
+			}
 		}
 	}
 
