96952e8259c1457b16eb16df4d228c58283404b8
oozzy <oozzy.tlo@gmail.com>
2016-02-21 18:31:47 -0800

    Add camera switch feature

---
+++ Assets/Scripts/Camera/Camera2Follow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera2Follow : MonoBehaviour {
+	public Rigidbody player;
+
+	// Use this for initialization
+	void Start () {
+		transform.rotation = new Quaternion (0, 180, 0, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = transform.position.x;
+		float y = transform.position.y;
+		float z = player.transform.position.z;
+		transform.position = new Vector3(x, y, z);
+	}
+}
