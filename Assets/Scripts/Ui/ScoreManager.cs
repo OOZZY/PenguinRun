65cce6ae6df2315779b909926b01d2a385408ca9
Dolapo Toki <dolapotoki@gmail.com>
2016-02-21 22:23:14 -0800

    added Scoreing mechanics

--- Assets/Scripts/Ui/ScoreManager.cs
+++ Assets/Scripts/Ui/ScoreManager.cs
@@ -4,24 +4,37 @@ using System.Collections;
 
 public class ScoreManager : MonoBehaviour {
     public static int score;        // The player's score.
-
-
     Text text;                      // Reference to the Text component.
 
+    public static float scoreCount;
+    public static float highscoreCount;
+
+    public float pointsPerSecond;
+
+    public bool scoreIncreasing;
 
     void Awake()
     {
         // Set up the reference.
         text = GetComponent<Text>();
-
-        // Reset the score.
         score = 0;
     }
 
 
     void Update()
     {
-        // Set the displayed text to be the word "Score" followed by the score value.
-        text.text = "Score: " + score;
+        if (scoreIncreasing)
+        {
+            scoreCount += pointsPerSecond * Time.deltaTime;
+        }
+
+        if (scoreCount > highscoreCount)
+        {
+            highscoreCount = scoreCount;
+        }
+
+        //scoreText.text = "" + Mathf.Round(scoreCount);
+        //highscoreText.text = "" + Mathf.Round(highscoreCount);
+        text.text = "Score: " + Mathf.Round(scoreCount); ;
     }
 }
